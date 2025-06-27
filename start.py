10ea2af3b816166101b9d2da020193375e1be387
hyejin032 <itw030254@naver.com>
2025-06-27 15:53:35 +0900

    0627
    ログインしてメイン画面に入る
    メイン画面を構成を考え、マイページに入ってユーザ情報を変更できるようにする。

--- start.py
+++ start.py
@@ -1,9 +1,67 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session
+import csv
+import os
+from werkzeug.security import generate_password_hash, check_password_hash
 
-app = Flask("JobScrapper")
+app = Flask("Subject Manager")
+app.secret_key = 'qwerty1234'
+CSV_FILE = 'users.csv'
 
-@app.route("/")
+# CSVからユーザーを読み込む
+def load_users():
+    users = {}
+    if os.path.exists(CSV_FILE):
+        with open(CSV_FILE, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                users[row['email']] = row['password']
+    return users
+
+# CSVにユーザーを書き込む
+def save_user(email, name, password, faculty):
+    exists = os.path.exists(CSV_FILE)
+    with open(CSV_FILE, 'a', newline='') as csvfile:
+        fieldnames = ['email', 'name', 'password', 'faculty']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        if not exists:
+            writer.writeheader()
+        writer.writerow({'email': email,
+            'name': name,
+            'password': password,
+            'faculty': faculty})
+
+@app.route('/')
 def home():
-  return 'Hello Flask!'
+    return render_template('home.html')
 
-app.run("0.0.0.0")
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        users = load_users()
+        if email in users:
+            session['user_email'] = email
+        return f"ログイン成功（{email}）"
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form['email']
+        name = request.form['name']
+        password = request.form['password']
+        faculty = request.form['faculty']
+        
+        if not email or not name or not password or not faculty:
+            return "すべて入力してください"
+        
+        with open('users.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([email, name, password, faculty])
+        
+        return f"サインアップ成功（{name}）"
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
